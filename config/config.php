14c18d6781a1e70c27804cd66d92981f91f3f8a9
Lucile Gentner <lucile.gentner@gmail.com>
2016-02-17 18:33:48 +0100

    Updating install files

--- config/config.php
+++ config/config.php
@@ -1,3 +1,11 @@
 <?php
 
+//Chemin du projet
+
 define('DIR_PATH', 'C:/wamp/www/POOExos/');
+
+//Base de données
+define('SERVER_NAME', 'localhost');
+define('DB_NAME', 'ecvdigital');
+define('DB_USR', 'root');
+define('DB_PWD', '');
--- install/install.php
+++ install/install.php
@@ -1,4 +1,46 @@
 <?php
 
-$db = new \PDO('mysql:host=localhost;dbname=ecvdigital', 'root', '');
+/*
+ * Install the database
+ */
+require_once '../config/config.php';
 
+//HTML
+include(DIR_PATH.'html/header.html');
+
+echo "<h1>Installation</h1>";
+
+try {
+    $dbh = new PDO('mysql:host='.SERVER_NAME.';dbname='.DB_NAME, DB_USR, DB_PWD);
+
+    $dbCreationQuery='';
+    $dbCreationQuery=file_get_contents ("ecvdigital.sql");
+    $stmt = $dbh->prepare($dbCreationQuery);
+    if ($stmt){
+        $res = $stmt->execute();
+        echo "<div>-->La base a correctement été installée</div>";
+    }else{
+        echo "<div>-->Erreur lors de l'installation de la base</div>";
+        die;
+    }
+    
+    $dataInsertionQuery=file_get_contents ("datatest.sql");
+
+    $stmt = $dbh->prepare($dataInsertionQuery);
+    if ($stmt){
+        $res = $stmt->execute();
+        echo "<div>-->Les données de test ont été correctement insérées</div>";
+    }else{
+        echo "<div>-->Erreur lors de l'insertion des données de test</div>";
+        die;
+    }
+
+}catch (PDOException $e){
+    print "Error !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+
+include(DIR_PATH.'/html/footer.html');
+?>
